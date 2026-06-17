705c2dc47ddfffceb813aad4221202c3da4c6b8f
Usman Shafiq <mu945639@gmail.com>
2026-06-17 14:54:09 +0500

    TEXT-BASED GAME PAGE COMPLETED

--- final_project.cpp
+++ final_project.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<fstream>
-#include<raylib.h>
+#include<windows.h>
 #include"mystr.h"
 using namespace std;
 class character {
@@ -41,12 +41,17 @@ public:
 			health = a;
 		}
 	}
+	virtual void display() {
+		cout << "NAME :" << name << endl;
+		cout << "HEALTH: " << health << endl;
+		cout << "ATTACKING POWER: " << attacking_power << endl;
+	}
 };
 int character::total_characters = 0;
                              // HERO CLASSES
 class GodOfThunder: public character {
 public:
-	GodOfThunder() : character("THOR",100,27){}
+	GodOfThunder() : character("THOR ",100,27){}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -60,11 +65,14 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
 class MysticArts : public character {
 public:
-	MysticArts() : character("DOCTOR STRANGE", 100, 30) {}
+	MysticArts() : character("DOCTOR STRANGE ", 100, 30) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -78,11 +86,14 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
 class ArmoredAvenger : public character {
 public:
-	ArmoredAvenger() : character("IRON-MAN", 100, 20) {}
+	ArmoredAvenger() : character("IRON-MAN ", 100, 20) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -96,11 +107,14 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
 class Archer : public character {
 public:
-	Archer() : character("HAWKAYE", 100, 17) {}
+	Archer() : character("HAWKAYE ", 100, 17) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -114,11 +128,14 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
 class SuperSoldier : public character {
 public:
-	SuperSoldier() : character("CAPTAIN-AMERICA", 100, 22) {}
+	SuperSoldier() : character("CAPTAIN-AMERICA ", 100, 22) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -132,11 +149,14 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
 class GammaRadiation : public character {
 public:
-	GammaRadiation() : character("HULK", 100, 24) {}
+	GammaRadiation() : character("HULK ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -150,12 +170,15 @@ public:
 	}
 	void healthgain(int h)override {
 		character::healthgain(h);
+	}
+	void display() {
+		character::display();
 	}
 };
                                        //   ENEMY CLASSES
 class ChitauriSoldier : public character {
 public:
-	ChitauriSoldier() : character("LOKI", 100, 24) {}
+	ChitauriSoldier() : character("LOKI ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -173,7 +196,7 @@ public:
 };
 class LivingAutomaton : public character {
 public:
-	LivingAutomaton() : character("ULTRON", 100, 24) {}
+	LivingAutomaton() : character("ULTRON ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -191,7 +214,7 @@ public:
 };
 class BlackDwarf : public character {
 public:
-	BlackDwarf() : character("CullObsibian", 100, 24) {}
+	BlackDwarf() : character("CullObsibian ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -209,7 +232,7 @@ public:
 };
 class Abonymow : public character {
 public:
-	Abonymow() : character("Abomination", 100, 24) {}
+	Abonymow() : character("Abomination ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -227,7 +250,7 @@ public:
 };
 class TitanWarLord : public character {
 public:
-	TitanWarLord() : character("THANOS", 100, 24) {}
+	TitanWarLord() : character("THANOS ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -245,7 +268,7 @@ public:
 };
 class TimeVariantKing : public character {
 public:
-	TimeVariantKing() : character("KANG", 100, 24) {}
+	TimeVariantKing() : character("KANG ", 100, 24) {}
 	int attack()override {
 		return chr_attacking_power();
 	}
@@ -337,5 +360,97 @@ public:
 	}
 };
 int main() {
+	bool flag = true, bulb = true;
+	int t = 0, a = 0;
+	character* hero = nullptr;
+	cout << "                 ..........WELCOME TO MARVEL BATTLE ARENA GAME..............." << endl;
+	cout << endl ;
+	cout << "    THERE ARE TOTAL 6 CHARACTERS IN THIS GAME." << endl<<endl;
+	cout << "    WHEREAS THE LEVEL IN THIS GAME ARE ALSO SIX IF YOU DEFEAT THE KANG IN THE FINAL LEVEL THEN YOU WILL WIN THIS GAME" << endl << endl;
+	cout << "   WHICH CHARACTER DO YOU WANT TO CHOOSE FOR GAME LEVELS" << endl;
+	while (flag == true) {
+		cout << "1. THOR (GOD OF THUNDER)" << endl;
+		cout << "2. DOCTOR STRANGE (MYSTIC ARTS)" << endl;
+		cout << "3. IRON-MAN (ArmoredAvenger)" << endl;
+		cout << "4. HAWKAYE (Archer)" << endl;
+		cout << "5. CAPTAIN AMERICA (SuperSoldier)" << endl;
+		cout << "6. HULK (GAMMA-RADIATION)" << endl;
+		cout << "ENTER YOUR INPUT" << endl;
+		cin >> t;
+		if (t < 1 || t>6) {
+			cout << "INVALID INPUT. TRY AGAIN" << endl;
+		}
+		else {
+			if (t == 1) {
+				 hero = new GodOfThunder;
+			}
+			else if (t == 2) {
+				 hero = new MysticArts;
+			}
+			else if (t == 3) {
+				 hero = new ArmoredAvenger;
+			}
+			else if (t == 4) {
+				 hero = new Archer;
+			}
+			else if (t == 5) {
+				 hero = new SuperSoldier;
+			}
+			else if (t == 6) {
+				 hero = new GammaRadiation;
+			}
+			cout <<endl<< "THE CHOSEN CHARACTER CHARACTERISTIC :" << endl;
+			hero[0].display();
+			flag = false;
+		}
+	}
+	while (flag == false) {
+		cout << "DO YOU WANT TO READ THE TUTORIAL OF THIS GAME (1.YES  AND  2.NO) :" << endl;
+		cin >> t;
+		if (t < 1 || t>2) {
+			cout << "INVALID INPUT. TRY AGAIN" << endl;
+		}
+		else {
+			if (t == 1) {
+				while (bulb == true) {
+					system("cls");
+					cout << "THIS GAME HAS 3 MODES: " << endl;
+					cout << "1. ATTACK:  Give normal damage to the enemy." << endl;
+					cout << "2 DEFEND:  Reduces the damage received from the enemy's next attack." << endl;
+					cout << "3. POWERBOOM:  Uses a special ability that deals higher damage than a normal attack." << endl;
+					cout << "YOU CAN PRESS ONE BUTTON AT ANY TIME IN ONE MINUTE ROUND OF EVERY LEVEL" << endl;
+					cout << "THE OPPONENT ALSO ATTACKS YOU OR DEFEND HIMSELF AT ANY TIME" << endl;
+					cout << endl;
+					cout << "           DO YOU UNDERSTAND THE TUTORIAL (1.YES  AND  2.NO)" << endl;
+					cin >> a;
+					if (a < 1 || a>2) {
+						cout << "INVALID INPUT. I MOVE TO THE GAME" << endl;
+						bulb = false;
+					}
+					else {
+						if (a == 2) {
+							cout << "READ AGAIN THE TUTORIAL" << endl;
+							Sleep(1000);
+						}
+						else {
+							cout << "NOW THE GAME HAS BEEN STARTED" << endl;
+							bulb = false;
+						}
+						
+					}
+				}
+				flag = true;
+			}
+			else if (t == 2) {
+				cout << "OK. THEN THE GAME HAS BEEN STARTED" << endl;
+				flag = true;
+			}
+		}
+	}
+	Sleep(500);
+	system("cls");
+	
+	
+	
 
 }
